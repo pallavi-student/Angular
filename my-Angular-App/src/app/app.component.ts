c252ccfb564a34099c09aa69a8424614885cae8a
pallavi15 <pallavigaharwar15@gmail.com>
2025-03-06 00:58:05 +0530

    [Pallavi] Implemented Data Binding

--- my-Angular-App/src/app/app.component.ts
+++ my-Angular-App/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-Angular-App';
+  message: string = "Hello from BridgeLabz"; //this is what get shown in message
 }
